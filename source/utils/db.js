30ef6afb10ad5274b3c11e5ff3f5e89e272fe270
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2020-12-15 09:16:04 +0700

    Xóa những hàm chưa có do copy

--- source/utils/db.js
+++ source/utils/db.js
@@ -5,22 +5,5 @@ const pool = mysql.createPool(mysql_opts);
 const promisePool = pool.promise();
 
 module.exports = {
-  load(sql) {
-    return promisePool.query(sql); // [rows, fields]
-  },
-
-  add(entity, table_name) {
-    const sql = `insert into ${table_name} set ?`;
-    return promisePool.query(sql, entity);
-  },
-
-  del(condition, table_name) {
-    const sql = `delete from ${table_name} where ?`;
-    return promisePool.query(sql, condition);
-  },
-
-  patch(new_data, condition, table_name) {
-    const sql = `update ${table_name} set ? where ?`;
-    return promisePool.query(sql, [new_data, condition]);
-  }
+  
 };
