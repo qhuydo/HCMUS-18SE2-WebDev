945a6a481d015a072d83b92dafff991189ba31ad
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2020-12-20 22:57:32 +0700

    change path of main view, add error page
    (+) error page
    (*) change path main.hbs
    (*) change some in register page

--- source/app.js
+++ source/app.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const routers = require('./routers/router');
-const path = require('path')
 const db = require('./database/db');
 require('express-async-errors');
-
+   
 const app = express();
+app.use(express.static('public'));
 require('./views/view.js')(app);
 app.use(morgan('dev'));
 app.use(express.urlencoded({
     extended: true
 }));
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static('public'));
 
 routers.setDBObject(db);
 app.use(routers.routes);
-
+app.use(function (req, res) {
+    res.status(500)
+    res.render('error', {
+        style:'error.css',
+    })
+});
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.send("Error");
-    /*res.render('500', {
-        layout: false
-    })*/
+    res.render('error', {
+        style:'error.css'
+    })
 });
 
 const PORT = 3000;
