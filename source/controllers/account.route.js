347e24a1065d9e01cbf9f9c247ed91de6a953577
Việt <thanhvietgl1210@gmail.com>
2021-01-04 17:43:58 +0700

    Change the bcrypt package to bcrpyt js
    Các thay đổi chính
    [*] package.json: thay package 'bcrypt' thành 'bcryptjs', do pakage bcrypt yêu cầu dependency khi cài đặt.
    [*] account.model.js, account.model.js: thay đổi package bcrypt sang bcryptjs.

--- source/controllers/account.route.js
+++ source/controllers/account.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const account = require('../models/account.model');
 const auth = require('../middlewares/auth.mdw');
 const missingKeys = require("../utils/otherFunction").missingKeys;
--- source/models/account.model.js
+++ source/models/account.model.js
@@ -1,5 +1,5 @@
 const db = require('../utils/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 async function selectAccountTable(table, username, email) {
