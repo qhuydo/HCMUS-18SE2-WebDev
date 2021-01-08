541a6afa112bd0673da4259f534ec18eeb14d7f0
Việt <thanhvietgl1210@gmail.com>
2021-01-08 23:24:23 +0700

    tạo route render db ở 'home'

--- source/controllers/home.route.js
+++ source/controllers/home.route.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const missingKeys = require("../utils/otherFunction").missingKeys;
 const searchCourse = require('../models/course.model').searchCourse;
 const searchCatergory = require('../models/category.model').searchCategory;
+const courseModel = require('../models/course.model');
 
 router.get('/', async(req, res) => {
 
-    res.render('home');
+    const [list,fields]= await courseModel.getAllCourse();
+
+    
+    res.render('home', {
+        courses:list,
+        empty: list.length ===0
+    });
     // console.log(req);
     // console.log(req.session.auth);
     // console.log(req.session.username);
